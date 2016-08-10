6a8eacf229608cc5d1969aae353bafbefe47a15f
Brice Tessier Huort <tessierhuort@gmail.com>
2016-08-10 15:11:48 +0200

    add views and stuff

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -21,5 +21,8 @@ before_action :set_animal, only: [:show, :new, :edit, :create]
 
   def set_animal
     @restaurant = Restaurant.find(params[:id])
+    # @restaurant.reviews <=> Review.where(restaurant_id: @restaurant.id)
+    # @review = Review.find(1)
+    # @review.restaurant <=> Restaurant.find(@review.restaurant_id)
   end
 end
