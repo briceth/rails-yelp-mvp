e2dd56c5ef1aac21081e86e71dd99663f0e9d6f8
Brice Tessier Huort <tessierhuort@gmail.com>
2016-08-10 13:48:10 +0200

    add column

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
-#   has_many :reviews, dependent: :destroy
-#   categories = %w ()
+  has_many :reviews, dependent: :destroy
+  validates :name, :phone_number, :address, presence: true
+  CATEGORIES = %w(chinese, italian, french, vietnamese)
+  validates :category, inclusion: { in: CATEGORIES }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810110243) do
+ActiveRecord::Schema.define(version: 20160810112842) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20160810110243) do
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "address"
   end
 
 end
